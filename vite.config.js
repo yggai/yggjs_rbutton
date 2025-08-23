de6588a7c77d66d1d07b0b77c346b339b937eb20
yggai <pygosuperman@outlook.com>
2025-08-23 09:58:08 +0800

    使用vite构建包

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.js'),
+      name: 'yggjs_rbutton',
+      fileName: (format) => `yggjs_rbutton.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});
+
