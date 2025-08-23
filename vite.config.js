1b520a12c57de99914d38f823bca845644ff8a0e
yggai <pygosuperman@outlook.com>
2025-08-23 10:04:21 +0800

    将css合并到组件中

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import cssInjectedByJs from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJs()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
